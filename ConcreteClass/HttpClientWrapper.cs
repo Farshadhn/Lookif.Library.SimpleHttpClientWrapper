62eb044094201a6ebf2341ebe949d3ec820c0bc2
Farshad <sfarshadhn@gmail.com>
2023-04-30 20:04:11 +0330

    Api result is removed from everywhere

--- ConcreteClass/HttpClientWrapper.cs
+++ ConcreteClass/HttpClientWrapper.cs
@@ -1,11 +1,13 @@
 ﻿using Lookif.Layers.WebFramework.Api;
+using Lookif.Library.Common.Exceptions;
 using Lookif.Library.Common.Utilities;
 using Lookif.Library.SimpleHttpClientWrapper.Interface;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 
 namespace Lookif.Library.SimpleHttpClientWrapper.ConcreteClass;
- 
+
 public class HttpClientWrapper : IHttpClientWrapper
 {
     private IHttpClientFactory HttpClientFactory { get; }
@@ -19,7 +21,7 @@ public class HttpClientWrapper : IHttpClientWrapper
         HttpClientFactory = httpClientFactory;
     }
 
-    public async Task<TReturnType> GetSpecificAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class
+    public async Task<TReturnType> GetAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class
     {
         var client = name switch { "" or null => HttpClientFactory.CreateClient(), _ => HttpClientFactory.CreateClient(name) };
 
@@ -27,63 +29,20 @@ public class HttpClientWrapper : IHttpClientWrapper
         using var response = await client.GetAsync(add, cancellationToken);
         var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
         if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
+            throw GetCorrespondingException(response.StatusCode, responseInString);
 
-        var responseInT = DeserializeObject<ApiResult<TReturnType>>(responseInString);
-        return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.Data;
+
+
+
+        var responseInT = DeserializeObject<TReturnType>(responseInString);
+        return responseInT;
+
+
     }
+ 
 
 
-
-    public async Task<TReturnType> GetSpecificAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class
-    {
-
-        var client = name switch { "" or null => HttpClientFactory.CreateClient(), _ => HttpClientFactory.CreateClient(name) };
-
-        PrepareClient(token, client);
-        using var response = await client.GetAsync(add, cancellationToken);
-        var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
-        if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
-
-        var responseInT = DeserializeObject<ApiResult<TReturnType>>(responseInString);
-        return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.Data;
-    }
-
-    public async Task<List<TReturnType>> GetAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "")
-    {
-        var client = name switch { "" or null => HttpClientFactory.CreateClient(), _ => HttpClientFactory.CreateClient(name) };
-
-        PrepareClient(token, client);
-        using var response = await client.GetAsync(add, cancellationToken);
-        var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
-        if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
-
-        var responseInT = DeserializeObject<ApiResult<List<TReturnType>>>(responseInString);
-        return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.Data;
-    }
-
-    public async Task<List<TReturnType>> GetAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "")
-    {
-        var client = name switch { "" or null => HttpClientFactory.CreateClient(), _ => HttpClientFactory.CreateClient(name) };
-
-        PrepareClient(token, client);
-        HttpRequestMessage request = new(HttpMethod.Get, client.BaseAddress + add);
-
-        request.Content = new StringContent(SerializeObject(Data),
-                                                    Encoding.UTF8,
-                                                    "application/Json");
-        using var response = await client.SendAsync(request, cancellationToken);
-        var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
-        if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
-
-        var responseInT = DeserializeObject<ApiResult<List<TReturnType>>>(responseInString);
-        return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.Data;
-    }
-
-
+ 
 
     public async Task PostAsync<TInputType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "")
     {
@@ -92,23 +51,33 @@ public class HttpClientWrapper : IHttpClientWrapper
         PrepareClient(token, client);
         StringContent stringContent = new(SerializeObject(Data), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync(add, stringContent, cancellationToken);
+        var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
         if (response.IsSuccessStatusCode is not true)
-            throw new Exception("PostAsync - add - Error");
+            throw GetCorrespondingException(response.StatusCode, responseInString);
     }
-    public async Task<TReturnType> PostAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType: class
+    public async Task<TReturnType> PostAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class
     {
         var client = name switch { "" or null => HttpClientFactory.CreateClient(), _ => HttpClientFactory.CreateClient(name) };
 
         PrepareClient(token, client);
         StringContent stringContent = new(SerializeObject(Data), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync(add, stringContent, cancellationToken);
-        if (response.IsSuccessStatusCode is not true)
-            throw new Exception("PostAsync - add - Error");
         var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
+        if (response.IsSuccessStatusCode is not true)
+            throw GetCorrespondingException(response.StatusCode, responseInString);
 
-        var responseInT = DeserializeObject<ApiResult<TReturnType>>(responseInString);
-        return responseInT.Data;
+         
+
+        var responseInT = DeserializeObject<TReturnType>(responseInString);
+        return responseInT;
     }
+
+
+
+
+
+
+
 
     public async Task<bool> PutAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class
     {
@@ -119,9 +88,9 @@ public class HttpClientWrapper : IHttpClientWrapper
         using var response = await client.PutAsync(add, stringContent, cancellationToken);
         var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
         if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
-        var responseInT = DeserializeObject<ApiResult<TReturnType>>(responseInString);
-        return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.IsSuccess;
+            throw GetCorrespondingException(response.StatusCode, responseInString);
+        int state = (int)response.StatusCode;
+        return state == 204 || state == 200 || state == 201;
     }
 
 
@@ -133,7 +102,7 @@ public class HttpClientWrapper : IHttpClientWrapper
         using var response = await client.PutAsync(add, null, cancellationToken);
         var responseInString = await response.Content.ReadAsStringAsync(cancellationToken);
         if (response.IsSuccessStatusCode is not true)
-            throw new Exception(responseInString);
+            throw GetCorrespondingException(response.StatusCode, responseInString);
         var responseInT = DeserializeObject<ApiResult<TReturnType>>(responseInString);
         return responseInT.IsSuccess is not true ? throw new Exception("IsNotSuccess") : responseInT.IsSuccess;
     }
@@ -144,6 +113,13 @@ public class HttpClientWrapper : IHttpClientWrapper
         if (token.HasValue())
             client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
     }
+    private Exception GetCorrespondingException(HttpStatusCode httpStatusCode, string response)
+          => httpStatusCode switch
+          {
+              HttpStatusCode.BadRequest => new BadRequestException(response),
+              HttpStatusCode.NotFound => new NotFoundException(response),
+              _ => new Exception(response)
+          };
 
 
     #endregion
--- Interface/IHttpClientWrapper.cs
+++ Interface/IHttpClientWrapper.cs
@@ -1,12 +1,9 @@
 ﻿namespace Lookif.Library.SimpleHttpClientWrapper.Interface;
 public interface IHttpClientWrapper
 {
-    Task<List<TReturnType>> GetAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "");
-    Task<List<TReturnType>> GetAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "");
+    Task<TReturnType> GetAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class;
     HttpClient GetHttpClient(string name);
-    Task<TReturnType> GetSpecificAsync<TReturnType>(string add, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class;
-    Task<TReturnType> GetSpecificAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class;
-
+   
     Task PostAsync<TInputType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "");
     Task<TReturnType> PostAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class;
     Task<bool> PutAsync<TInputType, TReturnType>(string add, TInputType Data, CancellationToken cancellationToken, string name, string token = "") where TReturnType : class;
